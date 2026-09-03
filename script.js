83a9750ec766fc9ef554844d7529de18b47e5ad0
thefakepepsi <madhavhero40@gmail.com>
2026-09-03 18:33:59 +0400

    Update fetch URL to use local network address

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function sendMessage() {
     input.value = "";
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch("http://192.168.x.x:5000/chat") , {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: text })
